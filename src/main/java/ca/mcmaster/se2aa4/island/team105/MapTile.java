28aee344db4073e2e438002ee1772df73c4b92a7
christinazzhang <christina.916.zhang@gmail.com>
2024-02-13 18:30:18 -0500

    maptile updated

--- src/main/java/ca/mcmaster/se2aa4/island/team105/MapTile.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team105/MapTile.java
@@ -1,5 +1,41 @@
 package ca.mcmaster.se2aa4.island.team105;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class MapTile {
-    
+    private List<String> creekIdentifier(JSONObject extras) {
+        JSONArray ids = extras.getJSONArray("creeks");
+        List<String> idlist = new ArrayList<String>();
+        if (ids.length() > 0) {
+            for (int i = 0; i < ids.length(); i++) {
+                idlist.add(ids.getString(i));
+            }
+            return idlist;
+        } 
+        return null;
+    }
+
+    private List<String> siteIdentifier(JSONObject extras) {
+        JSONArray ids = extras.getJSONArray("creeks");
+        List<String> idlist = new ArrayList<String>();
+        if (ids.length() > 0) {
+            for (int i = 0; i < ids.length(); i++) {
+                idlist.add(ids.getString(i));
+            }
+            return idlist;
+        } 
+        return null;
+    }
+
+    private String biomeType(JSONObject extras) {
+        String biome = extras.getString("biomes");
+        return biome;
+    }
+
+    private boolean scanned = false;
+
 }
