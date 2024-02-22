372050d17f94a175cdc18417a830f636c6cd7ebb
kevinnkimm <43616290+kevinnkimm@users.noreply.github.com>
2024-02-22 00:01:58 -0500

    refactored Explorer.java class

--- src/main/java/ca/mcmaster/se2aa4/island/team105/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team105/Explorer.java
@@ -1,63 +1,29 @@
 package ca.mcmaster.se2aa4.island.team105;
 
-import java.io.StringReader;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import eu.ace_design.island.bot.IExplorerRaid;
-import org.json.JSONObject;
-import org.json.JSONTokener;
 import ca.mcmaster.se2aa4.island.team105.Drone.BatteryLevel;
-import ca.mcmaster.se2aa4.island.team105.Drone.Limitations;
+
 
 public class Explorer implements IExplorerRaid {
 
-    private final Logger logger = LogManager.getLogger();
     private BatteryLevel level;
-    private Limitations limitations;
+    JSONConfiguration initialize = new JSONConfiguration();
 
     @Override
     public void initialize(String s) {
-        logger.info("** Initializing the Exploration Command Center");
-        JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
-        logger.info("** Initialization info:\n {}", info.toString(2));
-        String direction = info.getString("heading");
-        this.level = new BatteryLevel(info.getInt("budget"));  // Create the BatteryLevel object
-        logger.info("The drone is facing {}", direction);
-        logger.info("Battery level is {}", this.level.getLevel());
-        this.limitations = new Limitations(this.level);
-
+        initialize.initializationWrap(s, level);
     }
+
     // not coded
     @Override
-    public String takeDecision() {
-        JSONObject decision = new JSONObject();
-        JSONObject parameter = new JSONObject();
-        decision.put("action", "heading");
-        parameter.put("direction", "S");
-        decision.put("parameters", parameter);
-        logger.info("** Decision: {}", decision.toString());
-
-        // decrement battery level for each iteration
-        logger.info("Battery level is now {}", this.level.getLevel());
-        return decision.toString();
+    public String takeDecision () {
+        return initialize.takeDecisionWrap(level);
     }
 
     
     @Override
     public void acknowledgeResults(String s) {
-        JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
-        logger.info("** Response received:\n" + response.toString(2));
-        Integer cost = response.getInt("cost");
-        logger.info("The cost of the action was {}", cost);
-        // battery level after receiving results
-        this.level.setLevel(this.level.getLevel() - cost);
-        logger.info("Battery level is now {}", this.level.getLevel());
-        String status = response.getString("status");
-        logger.info("The status of the drone is {}", status);
-        JSONObject extraInfo = response.getJSONObject("extras");
-        logger.info("Additional information received: {}", extraInfo);
-        limitations.returnHome();
+        initialize.acknowledgeResultsWrap(s);
     }
 
     @Override
--- src/main/java/ca/mcmaster/se2aa4/island/team105/JSONConfiguration.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team105/JSONConfiguration.java
@@ -2,19 +2,18 @@ package ca.mcmaster.se2aa4.island.team105;
 
 import org.json.JSONObject;
 import org.json.JSONTokener;
-
+import ca.mcmaster.se2aa4.island.team105.Drone.BatteryLevel;
 import ca.mcmaster.se2aa4.island.team105.Enums.Decision;
-
-import java.io.StringReader;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.io.StringReader;
 
 public class JSONConfiguration {
     
     private final Logger logger = LogManager.getLogger();
     JSONObject decision = new JSONObject();
     JSONObject parameter = new JSONObject();
+    private BatteryLevel level;
 
     
     public String decisionWrap(Decision takeDecision) {
@@ -62,4 +61,41 @@ public class JSONConfiguration {
         return null;
 
     }
+
+    public void initializationWrap(String s, BatteryLevel level) {
+        logger.info("** Initializing the Exploration Command Center");
+        JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
+        logger.info("** Initialization info:\n {}", info.toString(2));
+        String direction = info.getString("heading");
+        this.level = new BatteryLevel(info.getInt("budget"));  // Create the BatteryLevel object
+        logger.info("The drone is facing {}", direction);
+        logger.info("Battery level is {}", this.level.getLevel());
+    }
+
+    public String takeDecisionWrap(BatteryLevel level) {
+        JSONObject decision = new JSONObject();
+        JSONObject parameter = new JSONObject();
+        decision.put("action", "heading");
+        parameter.put("direction", "S");
+        decision.put("parameters", parameter);
+        logger.info("** Decision: {}", decision.toString());
+        // decrement battery level for each iteration
+        logger.info("Battery level is now {}", this.level.getLevel());
+        return decision.toString();
+    }
+
+    public void acknowledgeResultsWrap(String s) {
+        JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
+        logger.info("** Response received:\n" + response.toString(2));
+        Integer cost = response.getInt("cost");
+        logger.info("The cost of the action was {}", cost);
+        // battery level after receiving results
+        this.level.setLevel(this.level.getLevel() - cost);
+        logger.info("Battery level is now {}", this.level.getLevel());
+        String status = response.getString("status");
+        logger.info("The status of the drone is {}", status);
+        JSONObject extraInfo = response.getJSONObject("extras");
+        logger.info("Additional information received: {}", extraInfo);
+    }
+
 }
