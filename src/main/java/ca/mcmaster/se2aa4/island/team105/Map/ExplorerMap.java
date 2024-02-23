9909ba9be6a57d96893236c4263d533a1e4206f0
Victor Yu <victorthelord123@gmail.com>
2024-02-22 19:04:08 -0500

    Update ExplorerMap.java
    Added MVP for Explorer Map with simple 2d array, stores MapTile, sets and gets by location (row, col)

--- src/main/java/ca/mcmaster/se2aa4/island/team105/Map/ExplorerMap.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team105/Map/ExplorerMap.java
@@ -1,5 +1,20 @@
 package ca.mcmaster.se2aa4.island.team105.Map;
 
+
 public class ExplorerMap {
-    
+    private MapTile[][] map;
+
+    public ExplorerMap(int height, int width) {
+        map = new MapTile[height][width];
+    }
+
+    public void setLocation(int row, int col, MapTile tile) {
+        map[row][col] = tile;
+    }
+
+    public MapTile getLocation(int row, int col) {
+        return map[row][col];
+    }
+
+
 }
