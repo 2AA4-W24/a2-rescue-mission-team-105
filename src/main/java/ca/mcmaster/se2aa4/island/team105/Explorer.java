9d31af75e0934f99b6d505c0222976a9c2fdd3f1
christinazzhang <christina.916.zhang@gmail.com>
2024-03-01 00:10:13 -0500

    mini change

--- src/main/java/ca/mcmaster/se2aa4/island/team105/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team105/Explorer.java
@@ -2,14 +2,12 @@ package ca.mcmaster.se2aa4.island.team105;
 
 import eu.ace_design.island.bot.IExplorerRaid;
 import ca.mcmaster.se2aa4.island.team105.Drone.BatteryLevel;
-import ca.mcmaster.se2aa4.island.team105.Map.MapTile;
 
 
 public class Explorer implements IExplorerRaid {
 
     private BatteryLevel level;
     JSONConfiguration initialize = new JSONConfiguration();
-    MapTile test = new MapTile();
 
     @Override
     public void initialize(String s) {
