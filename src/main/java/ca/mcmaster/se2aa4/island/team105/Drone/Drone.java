679c0cea6adc5364d14d4a233a28b2b3d96aea82
Victor Yu <victorthelord123@gmail.com>
2024-03-02 15:08:12 -0500

    Update Drone class with turning
    Sets the heading to the correct orientation when we turn.

--- src/main/java/ca/mcmaster/se2aa4/island/team105/Drone/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team105/Drone/Drone.java
@@ -7,6 +7,10 @@ import java.nio.file.Paths;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.Map;
+import java.util.EnumMap;
+
+
 import ca.mcmaster.se2aa4.island.team105.Enums.Direction;
 
 public class Drone {
@@ -36,6 +40,22 @@ public class Drone {
     //Simple return to check the functionality
     public Direction getHeading() {
         return(this.heading);
+    }
+    public void headRight(){
+        Map<Direction, Direction> goingRight = new EnumMap<>(Direction.class);
+        goingRight.put(Direction.N, Direction.E);
+        goingRight.put(Direction.E, Direction.S);
+        goingRight.put(Direction.S, Direction.W);
+        goingRight.put(Direction.W, Direction.N);
+        this.heading = goingRight.get(this.heading);
+    }
+    public void headLeft(){
+        Map<Direction, Direction> goingLeft = new EnumMap<>(Direction.class);
+        goingLeft.put(Direction.N, Direction.W);
+        goingLeft.put(Direction.W, Direction.S);
+        goingLeft.put(Direction.S, Direction.E);
+        goingLeft.put(Direction.E, Direction.N);
+        this.heading = goingLeft.get(this.heading);
     }
     
 }
