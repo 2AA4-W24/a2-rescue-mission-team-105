3b667de6e4ff70fe938b7867a55ff4327d1b7918
kevinnkimm <43616290+kevinnkimm@users.noreply.github.com>
2024-03-03 14:32:42 -0500

    implemented bad command method and 180 degree checker

--- src/main/java/ca/mcmaster/se2aa4/island/team105/Drone/Limitations.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team105/Drone/Limitations.java
@@ -1,17 +1,27 @@
 package ca.mcmaster.se2aa4.island.team105.Drone;
 
+import java.util.EnumMap;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import ca.mcmaster.se2aa4.island.team105.Configuration.JSONConfiguration;
+import ca.mcmaster.se2aa4.island.team105.Enums.Direction;
+import java.util.Map;
 
 public class Limitations extends JSONConfiguration {
     private BatteryLevel level;
     private final Logger logger = LogManager.getLogger();
+    Drone drone = new Drone("E");
+    Direction heading = drone.getHeading();
 
     // Constructor to initialize Limitations with a BatteryLevel instance
     public Limitations(BatteryLevel level) {
         this.level = level;
+    }
+
+    public Limitations() {
+        //TODO Auto-generated constructor stub
     }
 
     // checks battery
@@ -25,9 +35,29 @@ public class Limitations extends JSONConfiguration {
     }
 
     // checks bad command
-    public void badCommand(Actions action) {
-        
+    public void badCommand(Actions action, Direction desiredDirection, Drone turning) {
+        if (desiredDirection == heading) {
+            logger.info("Already heading in the desired direction. Continuing exploration.");
+        } else {
+            if (is180DegreeTurn(desiredDirection)) {
+                logger.error("Attempting a 180-degree turn. Invalid command.");
+            } else {
+                action.heading(decision, parameter, desiredDirection);
+            }
+        }
     }
+
+    public boolean is180DegreeTurn(Direction desiredDirection) {
+        Map<Direction, Direction> oppositeDirections = new EnumMap<>(Direction.class);
+        oppositeDirections.put(Direction.N, Direction.S);
+        oppositeDirections.put(Direction.S, Direction.N);
+        oppositeDirections.put(Direction.E, Direction.W);
+        oppositeDirections.put(Direction.W, Direction.E);
+
+        return oppositeDirections.get(heading) == desiredDirection;
+    }
+
+
 
     // checks radio range
 
