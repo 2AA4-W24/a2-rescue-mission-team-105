ea272dabfdf98019e5d268b52d48cee8bde6dfc4
Victor Yu <victorthelord123@gmail.com>
2024-03-04 21:32:12 -0500

    limitation discription and map changes

--- src/main/java/ca/mcmaster/se2aa4/island/team105/Map/ExplorerMap.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team105/Map/ExplorerMap.java
@@ -2,20 +2,25 @@ package ca.mcmaster.se2aa4.island.team105.Map;
 
 
 public class ExplorerMap {
-    private MapTile[][] map;
+    private String[][] map;
+
+    //relative position this is starting state
+    private int x, y = 0; //regular cartesian grid style coords
+
+
     //Creates a 2d array that stores MapTile Objects
     public ExplorerMap(int height, int width) {
-        map = new MapTile[height][width];
+        map = new String[height][width];
     }
 
     //Sets a object to inputed location
-    public void setLocation(int row, int col, MapTile tile) {
-        map[row][col] = tile;
+    public void setLocation(int xdist, int ydist, String type) {
+
     }
 
     //gets object from location
-    public MapTile getLocation(int row, int col) {
-        return map[row][col];
+    public String getLocation(int xdist, int ydist) {
+        return "works";
     }
 
 
