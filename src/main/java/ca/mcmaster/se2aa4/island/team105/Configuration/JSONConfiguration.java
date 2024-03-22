e417f785171178ff5096512772c2d8ea11f2785f
christinazzhang <christina.916.zhang@gmail.com>
2024-03-22 15:55:54 -0400

    ??

--- src/main/java/ca/mcmaster/se2aa4/island/team105/Configuration/JSONConfiguration.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team105/Configuration/JSONConfiguration.java
@@ -47,9 +47,9 @@ public class JSONConfiguration {
 
     public String takeDecisionWrap() {
         // decisionMaker.findMapBox(limitation, level, direction, action, parameter);
-        decisionMaker.gridSearch(action, level, limitation, direction, parameter);
+        ((DecisionMaker)decisionMaker).gridSearch(action, level, limitation, direction, parameter);
         // decision = action.scan();
-        decision = decisionMaker.getDecision();
+        decision = ((DecisionMaker)decisionMaker).getDecision();
         logger.info(level.getX() + " " + level.getY());
         logger.info("** Decision: {}", decision.toString());
         logger.info("Battery level is now {}", this.level.getLevel());       
