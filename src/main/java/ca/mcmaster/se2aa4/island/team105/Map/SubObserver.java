fd1f3440a52d77853a35befd20cae9b3b08b1b1e
christinazzhang <christina.916.zhang@gmail.com>
2024-03-23 22:07:21 -0400

    comments

--- src/main/java/ca/mcmaster/se2aa4/island/team105/Map/SubObserver.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team105/Map/SubObserver.java
@@ -2,7 +2,7 @@ package ca.mcmaster.se2aa4.island.team105.Map;
 
 import org.json.JSONArray;
 
-public class SubObserver {
+public class SubObserver { // Superclass to hold subobserver methods
     public void update(String found, int range, JSONArray biomes, int batteryLevel) {
 
     }
--- src/main/java/ca/mcmaster/se2aa4/island/team105/Map/TranslateSubject.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team105/Map/TranslateSubject.java
@@ -2,7 +2,7 @@ package ca.mcmaster.se2aa4.island.team105.Map;
 
 import org.json.JSONObject;
 
-public class TranslateSubject {
+public class TranslateSubject { // Superclass to hold all Subject methods
     public void addObserver(SubObserver subObserver) {
 
     }
